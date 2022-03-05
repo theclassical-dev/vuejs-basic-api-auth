dce063e7e77d1bc26278ee0ebbf9592139fd5197
Abraham Peter <theclassical001@gmail.com>
2022-03-05 13:16:49 +0100

    token is stored in localstorage

--- src/store/subscriber.js
+++ src/store/subscriber.js
@@ -7,8 +7,11 @@ store.subscribe((mutation) => {
         console.log(mutation.payload)
         if(mutation.payload){
             axios.defaults.headers.common['Authorization'] = `Bearer ${mutation.payload}`
+            localStorage.setItem('token', mutation.payload)
         }else{
             axios.defaults.headers.common['Authorization'] = null
+            localStorage.removeItem('token')
+
         }
         break;
     }
