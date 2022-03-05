057a97f62d476e0dbf7c6d11250ee332c89e8b35
Abraham Peter <theclassical001@gmail.com>
2022-03-05 21:56:48 +0100

    api flickering prevented

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ require('@/store/subscriber')
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 
-store.dispatch('auth/attempt', localStorage.getItem('token'))
-
+store.dispatch('auth/attempt', localStorage.getItem('token')).then(() => {
+    
 createApp(App).use(store).use(router).mount('#app')
+
+})
+
